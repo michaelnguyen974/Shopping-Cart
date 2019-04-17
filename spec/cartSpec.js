d798c76a2c45ee9aebbffa7c1e8252ca8f9dd553
Michael Nguyen <michaelnguyen974@gmail.com>
2019-04-17 12:13:26 +0100

    Test Green: Cart now calculates total

--- spec/cartSpec.js
+++ spec/cartSpec.js
@@ -3,13 +3,17 @@ var cart
 var item1
   beforeEach(() => {
     cart = new Cart
-    item1 = { id: 0, 
-    name: "Almond Toe Court Shoes, Patent Black", 
-    category: "Women’s Footwear",
+    item1 = { id: 0, name: "Almond Toe Court Shoes, Patent Black", category: "Women’s Footwear",
     price: 99.00,
     stock: 5
     }
+    item2 = { id: 1, name: "Suede Shoes, Blue", category: "Women’s Footwear",
+    price: 42.00,
+    stock: 4
+    }
   })
+
+
   it("cart has an array of items", () => {
     expect(cart.cartArray).toEqual([])
   })
@@ -23,6 +27,13 @@ var item1
     price: 99.00,
     stock: 5
     }])
+  })
+
+  it("Checks for total in the cart", () => {
+    cart.add(item1)
+    cart.add(item2)
+    cart.calculateTotal()
+    expect(cart.total).toEqual(141.00)
   })
 
 
--- src/cart.js
+++ src/cart.js
@@ -1,11 +1,18 @@
 class Cart {
   constructor() {
     this.cartArray = []
+    this.total = 0
   }
 
   add(item) {
     this.cartArray.push(item)
   }
 
-  
+  calculateTotal() {
+    this.cartArray.forEach( (value, index, array) => {
+      return this.total += value.price
+    })
+    
+  }
+
 }
